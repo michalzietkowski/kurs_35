fc454a2e7d19ae5ab9b6fb308fa77a2a65f94c9d
michalzietkowski <michalzietkowski>
2025-05-08 20:24:04 +0200

    zajecia 5

---
+++ zajecia_5/slowniki.py
@@ -0,0 +1,13 @@
+# uzytkownik = ["Michał", "Ziętkowski", 20, True]
+
+uzytkownik = {
+    "imie": "Michał",
+    "nazwisko": "Ziętkowski",
+    "wiek": 20,
+    "czy_uczen": True
+}
+print(uzytkownik)
+
+print(uzytkownik["imie"])
+# print(uzytkownik["plec"])
+print(uzytkownik.get("plec"))
